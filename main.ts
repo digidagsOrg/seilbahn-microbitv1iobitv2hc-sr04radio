c3a1763238005f0dd1940be27b419b27ec804f72
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2026-05-05 10:02:19 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
-let distance = 0
-let angekommen = 0
-radio.setGroup(11)
-basic.forever(function () {
-    basic.pause(500)
+function setDistance () {
     // P0 = Trigger
     pins.digitalWritePin(DigitalPin.P0, 0)
     control.waitMicros(4)
     pins.digitalWritePin(DigitalPin.P0, 1)
     control.waitMicros(10)
     // P1 = Echo
-    distance = 34 * (pins.pulseIn(DigitalPin.P1, PulseValue.High) / 2000)
+    read_distance = 34 * (pins.pulseIn(DigitalPin.P1, PulseValue.High) / 2000)
+    read_distance = Math.round(read_distance)
+    return read_distance
+}
+let distance = 0
+let read_distance = 0
+let arrived = 0
+radio.setGroup(11)
+basic.forever(function () {
+    basic.pause(500)
+    distance = setDistance()
     serial.writeValue("distance", distance)
-    if (distance <= 5 && angekommen == 0) {
-        angekommen = 1
+    if (distance <= 5 && arrived == 0) {
+        arrived = 1
         basic.showNumber(1)
         // Seilbahn ist da
         radio.sendNumber(1)
     } else {
-        angekommen = 0
+        arrived = 0
         basic.showNumber(0)
     }
 })
